851b8e25d84f47d53945da39b7fd00b0ca9bad61
Juan Diego Quintero <juandiegoo18@gmail.com>
2025-05-08 19:36:33 -0500

    Update variables.py

--- Grupo_6/variables.py
+++ Grupo_6/variables.py
@@ -1,15 +1,21 @@
-HolaMundo = "Hola, mundo"
+HolaMundo = "Hola, mundo!"
 print (HolaMundo)
+
 print("-------------------------------")
+
 Nombre = "Jun Diego"
 edad = 37
 print (f"Mi nombre es {Nombre} y tengo {edad} años.")
 
 print("-------------------------------")
 
-
+print("Hola ", end=" ")
+print ("mundo!")
 
 print("-------------------------------")
 
+Nombre = "Jun Diego"
+edad = 37
+print (Nombre, edad, sep = " - ")
 
 print("-------------------------------")
