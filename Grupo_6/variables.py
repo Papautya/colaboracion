4c9bca0151665ed262a1f53d718dc2fbdff9896d
Juan Diego Quintero <juandiegoo18@gmail.com>
2025-05-08 20:38:00 -0500

    Update variables.py
    Se realiza ejemplos de diferentes tipos de variables con sus datos e Impresiones

--- Grupo_6/variables.py
+++ Grupo_6/variables.py
@@ -19,3 +19,66 @@ edad = 37
 print (Nombre, edad, sep = " - ")
 
 print("-------------------------------")
+
+# Ejemplo de declaracion y asignacion de variables
+Nombre  = "Juan Diego" # Una variable de Cadena
+Edad = 37              # Una variable de Entero
+Altura = 1.75          # Una variable de punto Flotante
+es_estudiante = True   # Una variable Booleana
+
+print("-------------------------------")
+
+# Asignacion de valores en la declaracion
+ciudad = "Colombia"
+poblacion = "45000000"
+
+# Re-Asignacion de valores
+ciudad = "Manizales"
+poblacion = "50000"
+
+print("-------------------------------")
+
+# Convenciones de nombres
+NombreCompleto = "Juan Diego Quintero Sanchez"
+TemperaturaActual = 23.5
+
+# Tipos de variables Dinamicos
+VariableDinamica = 10
+print(VariableDinamica) # imprime 10
+
+VariableDinamica = "Hola!"
+print(VariableDinamica) # imprime Hola!
+
+print("-------------------------------")
+
+# Enteros
+EnteroPositivo = 10
+EnteroNegativo = -10 
+
+# Flotantes
+FlotantePositivo = 10
+FlotanteNegativo = -0.10
+
+# Operaciones con Enteros y Flotantes
+ResultadoSuma = EnteroPositivo + FlotantePositivo
+ResultadoMultiplicacion = EnteroNegativo * FlotanteNegativo
+
+#Impresion de Resultados
+print(ResultadoSuma)
+print(" ")
+print(ResultadoMultiplicacion)
+
+print("-------------------------------")
+
+#Cadenas
+CadenaSimple = 'Hola, Mundo!'
+CadenaDoble = "Python es Dicertido "
+
+# Concatenacion de Cadenas
+SaludoCompleto = CadenaSimple + ' ' + CadenaDoble
+
+print(SaludoCompleto)
+
+print("-------------------------------")
+print("--------------FIN--------------")
+print("-------------------------------")
